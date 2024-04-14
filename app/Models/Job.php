624cab1c744e0616ebcb15c0334f1b549d7edc8c
Goronet <goronet@gmail.com>
2024-04-14 23:16:45 +0200

    Forms and CSRF Explained (with Examples)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,9 +11,11 @@ class Job extends Model
 {
     use HasFactory;
 
+
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $guarded = [];
+
 
     public function employer(): BelongsTo
     {
