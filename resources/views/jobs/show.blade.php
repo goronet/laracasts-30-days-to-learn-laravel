477229284868a60c3affc3074edf95b574e3cf21
Goronet <goronet@gmail.com>
2024-04-15 19:54:50 +0200

    Editing, Updating, and Deleting a Resource

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,5 +8,9 @@
     <p>
         This job pays {{ $job['salary'] }} per year.
     </p>
+
+    <div class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit</x-button>
+    </div>
 </x-layout>
 
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->cursorPaginate(3);
 
@@ -16,10 +17,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -35,10 +38,44 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Delete
+Route::delete('/jobs/{id}', function ($id) {
+    // Authorize (On hold..)
+
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
